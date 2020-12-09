5f9d08b56982c2ddd1e11c6a42a6d22bf2cd9032
Patrick <pworthey007@msn.com>
2020-12-09 14:22:09 -0800

    Initial commit

---
+++ Endpoint.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace snowglobe_endpoint_func
+{
+  public static class Endpoint
+    {
+        [FunctionName("Endpoint")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [EventHub("telemetryEventHubxv6ty3w67q4mmns/telemetryeventhubxv6ty3w67q4mm")]IAsyncCollector<string> outputEvents,
+            ILogger log)
+        {
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            if (data != null) {
+              await outputEvents.AddAsync(JsonConvert.SerializeObject(data));
+            }
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
